a656e99b695f736214e0beab6414b37543b659a7
HemantAntony <hemantantony@outlook.com>
2024-07-13 19:24:51 +0530

    Added html elements

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div className="footer">
+            <a id="twitter"></a>
+            <a id="facebook"></a>
+            <a id="instagram"></a>
+            <a id="github"></a>
+        </div>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,30 @@
+
+export default function Main() {
+    return (
+        <div className="main">
+            <img alt="Profile picture" />
+            <div className="Title">
+                <h1>Hemant Antony</h1>
+                <h2>Frontend Developer</h2>
+                <h3>hemantantony.com</h3>
+            </div>
+            <div className="main-buttons">
+                <button>Email</button>
+                <button>LinkedIn</button>
+            </div>
+            <div className="description">
+                <h2>About</h2>
+                <p>
+                    Hello, I'm Hemant Antony. I've been programming since my childhood.
+                    I'm fueled by my hunger to solve problems in the world and to make
+                    the world a better place for everyone.
+                </p>
+                <h2>Interests</h2>
+                <p>
+                    I love programming. Building my own application gives immense
+                    satisfaction. I also love making music and playing games.
+                </p>
+            </div>
+        </div>
+    );
+}
